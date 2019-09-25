642a42310eed077c2465f240e94f475214cdd402
Rhuam Sena <rhuam@outlook.com>
2019-09-25 16:10:06 -0300

    Init Commit

---
+++ src/views/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      < div
+    className = "animated fadeIn" >
+      Hello
+    world
+    !
+    < /div>
+  )
+    ;
+  }
+}
+
+export default Dashboard;
